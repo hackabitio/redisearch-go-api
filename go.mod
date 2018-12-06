b2c5141f7a8446c68b6a8fb79c46d70e89e3b25d
Foad Yousefi <foadyousefi@gmail.com>
2018-12-07 00:43:42 +0100

    Add chi and example route

--- go.mod
+++ go.mod
@@ -1,3 +1,6 @@
 module github.com/7kmCo/redisearch-go-api
 
-require github.com/gomodule/redigo v2.0.0+incompatible
+require (
+	github.com/go-chi/chi v3.3.3+incompatible
+	github.com/gomodule/redigo v2.0.0+incompatible
+)
--- main.go
+++ main.go
@@ -2,12 +2,37 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"encoding/json"
+
 	"github.com/7kmCo/redisearch-go-api/redisearch"
+	"github.com/go-chi/chi"
 )
 
 func main() {
-	c := redisearch.NewClient("localhost:6379", "myIndex")
-	docs, total, err := c.Search(redisearch.NewQuery("lorem").Limit(0, 2))
+		r := chi.NewRouter()
+	r.Get("/search/{query}", searchHandler)
+	http.ListenAndServe(":8080", r)
+}
 
+func searchHandler(w http.ResponseWriter, r *http.Request){
+	query := chi.URLParam(r, "query")
+	
+	c := redisearch.NewClient("localhost:6379", "myIndex")
+	docs, total, err := c.Search(redisearch.NewQuery(query).Limit(0, 10))
+	
+	if err != nil {
+		return
+	}
+	
 	fmt.Println(docs[0].Id, docs[0].Properties["post_title"], total, err)
+
+	js, err := json.Marshal(docs)
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+	}
+	
+	w.Write(js)
+	
 }
