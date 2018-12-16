0dc86bd14b5725a6bd93ff7635c653fafa47b5a4
Foad Yousefi <foadyousefi@gmail.com>
2018-12-17 00:44:19 +0100

    Create method for Spell Check

---
+++ redisearch/spellcheck.go
@@ -0,0 +1,61 @@
+package redisearch
+
+import (
+	"strconv"
+
+	"github.com/gomodule/redigo/redis"
+)
+
+type SpellCheck struct {
+	Term 					string 					`json:"term"`
+	Suggestions 	[]SpellSuggestions		`json:"suggestions"`
+}
+
+type SpellSuggestions struct {
+	Suggestion 	string `json:"suggestion"`
+	Distance 		float64 `json:"distance"`
+}
+
+// SpellCheck, Performs spelling correction on a query,
+// returning suggestions for misspelled terms
+func (i *Client) SpellCheck(q *Query) (suggestions []SpellCheck, total int, err error) {
+	conn := i.pool.Get()
+	defer conn.Close()
+
+	args := redis.Args{i.name}
+	args = append(args, q.serialize()...)
+
+	res, err := redis.Values(conn.Do("FT.SPELLCHECK", args...))
+	if err != nil {
+		return
+	}
+
+	// Initiate return document interface
+	sugg := make([]SpellCheck, 0, len(res))
+	// Prepare the document to be returned
+		for _, v := range res {
+			sugg = append(sugg, loadTerms(v.([]interface{})))
+		}
+
+	return sugg, 1, err
+}
+
+
+func loadTerms(arr []interface{}) (SpellCheck) {
+	sugg := SpellCheck{}
+	sugg.Term = string(arr[1].([]uint8))
+	
+	for _, vv := range arr {
+		switch vv.(type) {
+		case []interface{}:
+			s := SpellSuggestions{}
+			for _, sv := range vv.([]interface{}) {
+				s.Suggestion = string(sv.([]interface{})[1].([]uint8))
+				sd, _ := strconv.ParseFloat(string(sv.([]interface{})[0].([]uint8)), 32)
+				s.Distance = sd
+				sugg.Suggestions = append(sugg.Suggestions, s)
+			}
+		}
+	}
+	return sugg
+}
